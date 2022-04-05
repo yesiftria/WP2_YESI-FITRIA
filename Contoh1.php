80f1e57061ab6d8315937e9c902f9312347c90e2
yesiftria <101692414+yesiftria@users.noreply.github.com>
2022-04-05 10:09:53 +0700

    Add files via upload
    Tugas pertemuan1

---
+++ Contoh1.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Contoh1 extends CI_Controller {
+
+    public function index(){
+        echo "<h1>Perkenalan </h1>";
+        echo "Nama Saya Yesi Fitria, Saya Tinggal Di lampung, Olahraga yang saya sukai adalah riding;)";
+    }
+
+}
+
+/* End of file Contoh1.php */
+
+
